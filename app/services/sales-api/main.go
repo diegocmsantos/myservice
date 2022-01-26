fe85f890e278e064f1479b552d09c92e841b1f5b
Diego Maia <diego.maia@olx.com>
2022-01-26 14:30:02 +0000

    4.3 logging support

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
-	"os/signal"
-	"runtime"
-	"syscall"
 
-	"go.uber.org/automaxprocs/maxprocs"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 // build is the git version of this program. It is set using build flags in the makefile.
@@ -16,21 +13,39 @@ var build = "develop"
 
 func main() {
 
-	// Set the correct number of threads for the service
-	// based on what is available either by the machine or quotas.
-	if _, err := maxprocs.Set(); err != nil {
-		fmt.Println("maxprocs: %w", err)
+	// Construct the application logger.
+	log, err := initLogger("SALES-API")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer log.Sync()
+
+	// Perform the startup and shutdown sequence.
+	if err := run(log); err != nil {
+		log.Errorw("startup", "ERROR", err)
 		os.Exit(1)
 	}
 
-	g := runtime.GOMAXPROCS(0)
+}
 
-	log.Printf("starting service build[%s] CPU[%d]", build, g)
-	defer log.Println("service ended")
+func run(log *zap.SugaredLogger) error {
+	return nil
+}
 
-	shutdown := make(chan os.Signal, 1)
-	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
-	<-shutdown
+func initLogger(service string) (*zap.SugaredLogger, error) {
+	config := zap.NewProductionConfig()
+	config.OutputPaths = []string{"stdout"}
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.DisableStacktrace = true
+	config.InitialFields = map[string]interface{}{
+		"service": service,
+	}
 
-	log.Println("stopping service")
+	log, err := config.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	return log.Sugar(), nil
 }
