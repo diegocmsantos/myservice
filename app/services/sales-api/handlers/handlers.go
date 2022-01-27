5c6e951d33d6befaf2ffebe714c0f40a10cc12e9
Diego Maia <diego.maia@olx.com>
2022-01-27 08:37:13 +0000

    6.1 custom router

--- app/services/sales-api/handlers/handlers.go
+++ app/services/sales-api/handlers/handlers.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/diegocmsantos/myservice/app/services/sales-api/handlers/debug/checkgrp"
 	"github.com/diegocmsantos/myservice/app/services/sales-api/handlers/v1/testgrp"
-	"github.com/dimfeld/httptreemux/v5"
+	"github.com/diegocmsantos/myservice/foundation/web"
 	"go.uber.org/zap"
 )
 
@@ -56,13 +56,13 @@ type APIMuxConfig struct {
 }
 
 // APIMux constructs a http.Handler with all application routes defined.
-func APIMux(cfg APIMuxConfig) *httptreemux.ContextMux {
-	mux := httptreemux.NewContextMux()
+func APIMux(cfg APIMuxConfig) *web.App {
+	app := web.NewApp(cfg.Shutdown)
 
 	tgh := testgrp.Handlers{
 		Log: cfg.Log,
 	}
-	mux.Handle(http.MethodGet, "/v1/test", tgh.Test)
+	app.Handle(http.MethodGet, "/v1/test", tgh.Test)
 
-	return mux
+	return app
 }
